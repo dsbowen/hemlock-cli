51b4d8da2b35b535fc6a6404833e9331a120043f
dsbowen <dsbowen@wharton.upenn.edu>
2020-01-01 23:04:48 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='hemlock-cli-dsbowen',
     version='0.0.1',
     author='Dillon Bowen',
     author_email='dsbowen@wharton.upenn.edu',
     description='Command line interface for Hemlock projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dsbowen/hemlock-cli",
     packages=setuptools.find_packages(),
     include_package_date=True,
     classifiers=[
@@ -13,7 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['Click'],
     entry_points='''
         [console_scripts]
         hlk=hlk:hlk
